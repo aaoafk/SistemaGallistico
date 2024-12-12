1dec47207847cad5916ae4c1d11f61ecda86eaed
Steven Fernandez <stevenfernandez@Stevens-MacBook-Air.local>
2024-12-12 10:02:35 -0400

    `edit` and `new` action for practicas

--- app/controllers/practicas_controller.rb
+++ app/controllers/practicas_controller.rb
@@ -1,6 +1,6 @@
 # app/controllers/practicas_controller.rb
 class PracticasController < ApplicationController
-  before_action :set_practica, only: [ :show, :destroy ]
+  before_action :set_practica, only: [ :show, :destroy, :edit]
 
   def index
     # For now, let's scope practices to a specific gallo if provided
@@ -20,18 +20,35 @@ class PracticasController < ApplicationController
     # The @practica instance variable is set by the before_action
   end
 
+  def edit
+    # The @practica instance variable is set by the before_action 
+  end
+
+  def create
+    @practica = Practica.new(practica_params)
+    
+    if @practica.save
+      redirect_to practicas_path, notice: 'Práctica creada exitosamente!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @practica.destroy
 
     respond_to do |format|
-      format.html { redirect_to practicas_path, notice: "Practica borrado exitosamente." }
+      format.html { redirect_to practicas_path, notice: "Practica borrado exitosamente!" }
     end
   end
 
   private
-
-
   def set_practica
     @practica = Practica.find(params[:id])
+  end
+  private
+
+  def practica_params
+    params.require(:practica).permit(:fecha, :duracion_en_minutos, :gallo_id)
   end
 end
